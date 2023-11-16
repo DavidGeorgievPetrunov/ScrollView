12667404a5982138a4cc63c603a68a873a61806a
David Georgiev Petrunov <dpetrunov@cifpfbmoll.eu>
2023-11-16 20:48:25 +0100

    El articulo ahora es un editText                           Añadido boton al linearLayour para activar y desactivar la capacidad para editar el texto

--- app/src/main/java/com/example/scrollview/MainActivity.java
+++ app/src/main/java/com/example/scrollview/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.scrollview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.article);
+        editText.setFocusable(false);
+    }
+
+    public void writableOnOff(View view) {
+        if (editText.isFocusable()  ){
+            editText.setFocusable(false);
+            editText.setFocusableInTouchMode(false);
+        } else {
+            editText.setFocusable(true);
+            editText.setFocusableInTouchMode(true);
+        }
+
+        //Tambien puedo usar el enabled pero esto afecta la apariencia del texto haciendolo mas dificil de leer
+        //if if (editText.isEnabled()  ){
+        //            editText.setEnabled(false);
+        //        } else {
+        //            editText.setEnabled(true);
+        //        }
+        Toast.makeText(this, "¡Botón clicado!", Toast.LENGTH_SHORT).show();
     }
 }
